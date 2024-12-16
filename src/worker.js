a9f3ebec7c69650e19cd5b4dcac5ad408cb306f0
Rohit Nair <puthenrohit@gmail.com>
2024-12-16 14:18:27 +0530

    added comments for work

--- src/worker.js
+++ src/worker.js
@@ -4,6 +4,13 @@ const Scraper = require('./index');
 async function processBatch(batch) {
   const batchResult = await Scraper.listImageUrls(batch, 2);
   // download the image logic here
+
+  /**
+   * Loop across batchResult
+   * Use axios to get the image as buffer
+   * Pass the buffer to sharp -> convert to webp and save on disk -> inside 'userImages' folder
+   * Will do the DB operations later 
+   */
   return batchResult;
 }
 
